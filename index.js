0e6f827f81f2172a9ceb68620cc26dbc5f2f7e53
MohdArif <arif112mohd@gmail.com>
2022-09-06 13:53:37 +0530

    more functionalty added

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 let addbtn = document.getElementsByClassName("todo-button")[0];
 let input = document.getElementsByClassName("todo-input")[0];
 let todos = document.getElementsByClassName("todos-container")[0];
-let count =0;
+addbtn.style.cursor="pointer"
 // todos.style.display= "flex"
 
 addbtn.addEventListener('click' ,function (){
+
+    if(input.value.length == 0){
+        alert("Input Field Can't be Empty")
+    }
+    else{
     
     let list = document.createElement('div');
     list.innerText = input.value;
@@ -13,6 +18,7 @@ addbtn.addEventListener('click' ,function (){
     list.style.color="white"
     todos.appendChild(list);
     let taskCompleted = document.createElement("button")
+    taskCompleted.style.cursor="pointer"
     taskCompleted.style.width= "150px"
     taskCompleted.style.height= "25px"
     taskCompleted.style.fontSize= "20px"
@@ -28,17 +34,20 @@ addbtn.addEventListener('click' ,function (){
         // list.style.textDecoration="line-through solid rbg(0,0,0)"
         if(list.className==="line-through"){
             list.classList.remove("line-through")
+            list.style.color="white"
             taskCompleted.innerText="Completed"
         
         }
         else{
             list.setAttribute('class','line-through')
         taskCompleted.innerText="Uncompleted"
+        list.style.color="green"
         }
     })
 
 
     let taskRemove = document.createElement("button")
+    taskRemove.style.cursor="pointer"
     taskRemove.innerText= "Remove"
     taskRemove.style.width= "120px"
     taskRemove.style.height= "25px"
@@ -46,9 +55,15 @@ addbtn.addEventListener('click' ,function (){
     todos.appendChild(taskRemove)
 
     taskRemove.addEventListener('click', function removeTask(){
+
+       let result= confirm("Are you sure")
+       if(result){
         list.style.display="none";
         taskCompleted.style.display="none";
         taskRemove.style.display="none";
+       }
     })
+}
 
 })
+
